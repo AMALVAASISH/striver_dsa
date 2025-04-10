13e07466f458327e5cafe30cd41e87b5d32e9e50
Amalaasish <139550073+AMALVAASISH@users.noreply.github.com>
2025-04-10 17:41:46 +0530

    second largest done

--- Arrays/easy/second_largest/Main.java
+++ Arrays/easy/second_largest/Main.java
@@ -1,6 +1,6 @@
 package Arrays.easy.second_largest;
 
-import java.util.Arrays;
+// import java.util.Arrays;
 // brute force approach
 // works if there are no duplicates
 
@@ -37,38 +37,99 @@ import java.util.Arrays;
 
 // better approach
 
+// public class Main{
+
+//     static void sorting(int[] arr, int num){
+//         if(num == 1 || num == 0){
+//             System.out.println("there is no array length");
+//         }
+//         int small = Integer.MAX_VALUE;
+//         int second_small = Integer.MAX_VALUE;
+//         int large = Integer.MIN_VALUE;
+//         int second_large = Integer.MIN_VALUE;
+//         for(int i = 0;i<num;i++){
+//             small = Math.min(small, arr[i]);
+//             large = Math.max(large,arr[i]);
+//         }
+//         for(int j = 0;j<num;j++){
+//             if(arr[j] < second_small && arr[j]!= small){
+//                 second_small = arr[j];
+//             }
+
+//             if(arr[j] > second_large && arr[j] != large){
+//                 second_large = arr[j];
+//             }
+//         }
+
+//         System.out.println(second_large);
+//         System.out.println(second_small);
+//     }
+//     public static void main(String[] args){
+//         int[] arr = {1,4,2,9,5};
+//         int n = arr.length;
+
+//         sorting(arr,n);
+//     }
+// }
+
+
+// Time Complexity: O(N), We do two linear traversals in our array
+
+// Space Complexity: O(1)
+
+
+
+// optimal approach
+
 public class Main{
 
-    static void sorting(int[] arr, int num){
-        if(num == 1 || num == 0){
-            System.out.println("there is no array length");
-        }
+    static int second_smallest(int arr[], int n){
         int small = Integer.MAX_VALUE;
         int second_small = Integer.MAX_VALUE;
+
+        System.out.println(small);// value: 2147483647
+
+        for(int i = 0;i<n;i++){
+            if(arr[i]< small){
+                second_small = small;
+                small = arr[i];
+
+            }else if(arr[i] < second_small && arr[i]!= small){
+                second_small = arr[i];
+            }
+        }
+        return second_small;
+    }
+
+
+    static int second_largest(int[] arr, int num){
         int large = Integer.MIN_VALUE;
         int second_large = Integer.MIN_VALUE;
+
         for(int i = 0;i<num;i++){
-            small = Math.min(small, arr[i]);
-            large = Math.max(large,arr[i]);
-        }
-        for(int j = 0;j<num;j++){
-            if(arr[j] < second_small && arr[j]!= small){
-                second_small = arr[j];
-            }
-
-            if(arr[j] > second_large && arr[j] != large){
-                second_large = arr[j];
+            if(arr[i]>large){
+                second_large = large;
+                large = arr[i];
+            }else if(arr[i]> second_large && arr[i]!= large){
+                second_large =arr[i];
             }
         }
 
-        System.out.println(second_large);
-        System.out.println(second_small);
+        return second_large;
     }
     public static void main(String[] args){
-        int[] arr = {1,4,2,9,5};
+        int[] arr = {1,4,2,8,6,0};
         int n = arr.length;
 
-        sorting(arr,n);
+        int ss = second_smallest(arr,n);
+        int ll = second_largest(arr, n);
+
+        System.out.println("second small:"+ ss);
+        System.out.println("second large:"+ ll);
     }
 }
 
+
+// Time Complexity: O(N), Single-pass solution
+
+// Space Complexity: O(1)
